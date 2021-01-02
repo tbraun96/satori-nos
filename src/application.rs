7fccba7c4ab0fa6a11e56d6caf2e1c20c5b4df17
tbraun96 <braunth@oregonstate.edu>
2021-01-01 19:41:02 -0500

    Added updater code

--- src/application.rs
+++ src/application.rs
@@ -4,19 +4,40 @@ use std::sync::Arc;
 use std::collections::HashMap;
 use std::sync::atomic::{Ordering, AtomicU64};
 use parking_lot::RwLock;
-use tokio::sync::mpsc::channel;
+use tokio::sync::mpsc::{channel, Sender};
+use serde::{Serialize, Deserialize};
+use crate::primitives::updater::VariableUpdater;
 
 /// Each application that gets run spawns in its own task associated with a session
 #[derive(Clone)]
 pub struct Application {
-    map: Arc<RwLock<HashMap<u64, NetworkVariableInner>>>,
+    /// These variables maintain a shared-state across the network
+    variables: Arc<RwLock<HashMap<u64, (Sender<NetworkUpdateState>, NetworkVariableInner)>>>,
     id_generator: Arc<AtomicU64>
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum ApplicationState {
+    Variable { state: VariableState, vid: u64, value: Vec<u8> }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum VariableState {
+    Init, Update
+}
+
+#[allow(variant_size_differences)]
+#[derive(Serialize, Deserialize)]
+pub enum NetworkUpdateState {
+    AllowRead { vid: u64 },
+    AllowWrite { vid: u64 },
+    ValueModified { vid: u64, value: Vec<u8> }
 }
 
 impl Application {
     pub fn new() -> Self {
         Self {
-            map: Arc::new(RwLock::new(HashMap::new())),
+            variables: Arc::new(RwLock::new(HashMap::new())),
             id_generator: Arc::new(AtomicU64::new(1))
         }
     }
@@ -33,10 +54,18 @@ impl Application {
     fn create_variable<T: NetworkTransferable>(&self, value: T, var_type: VariableType) -> NetworkVariable<T> {
         let (notifier_tx, notifier_rx) = channel::<()>(1);
         let (updater_tx, updater_rx) = channel::<OwnedGuard<T>>(1);
+        let (state_update_tx, state_update_rx) = channel(3);
         let net_var_inner = NetworkVariableInner::new::<T>(value, var_type, notifier_rx, updater_tx);
         let user_net_var = NetworkVariable::<T>::new(net_var_inner.clone());
-        let mut lock = self.map.write();
-        lock.insert(self.get_next_vid(), net_var_inner);
+
+        let variable_updater = VariableUpdater::<T>::new(state_update_rx, notifier_tx, net_var_inner.clone());
+        // TODO: updater_rx handler
+        let mut lock = self.variables.write();
+        lock.insert(self.get_next_vid(), (state_update_tx, net_var_inner));
+        // spawn the variable updater
+        tokio::task::spawn(async move {
+            variable_updater.await
+        });
         // register the variable with the internal local mapping
         user_net_var
     }
--- tests/primary.rs
+++ tests/primary.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use std::error::Error;
+    use satori_nos::application::Application;
 
     fn setup_log() {
         std::env::set_var("RUST_LOG", "info,error,warn,trace");
@@ -14,6 +15,9 @@ mod tests {
     #[tokio::test]
     async fn main() -> Result<(), Box<dyn Error>> {
         setup_log();
+        let app = Application::new();
+        let var0 = app.create_mutex_variable(String::from("Heya!"));
+        var0.write().await.unwrap().push('c');
         Ok(())
     }
 }
